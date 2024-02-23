519126cb29a4950c0fba7d40c9a7ab366a128ba2
Gustavo Soto Reyes <gsotoreyes73@gmail.com>
2024-02-23 03:40:37 -0500

    create project using data binding

---
+++ app/src/main/java/com/example/binding/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.binding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.binding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Person pp = new Person("GUS", "gus@gmail.com");
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setPerson(pp);
+    }
+}
---
+++ app/src/main/java/com/example/binding/Person.java
@@ -0,0 +1,30 @@
+package com.example.binding;
+
+public class Person {
+    String name;
+    String email;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
